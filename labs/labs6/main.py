62a3e9c7cfa71b1331ee39e8ae9de17dc78f47c1
Micah Dougall <micahd@tutamail.com>
2023-06-01 00:29:11 +0100

    Labs six

---
+++ labs/labs6/main.py
@@ -0,0 +1,24 @@
+def marginalize(
+        this_given_other: float, other: float, not_other_given_not_this: float
+) -> float:
+    return (
+        this_given_other * other
+        + ((1 - not_other_given_not_this) * (1 - other))
+    )
+
+
+def bayesian_probability(
+        likelihood: float, prior: float, marginalization: float
+) -> float:
+    return likelihood * prior / marginalization
+
+
+if __name__ == '__main__':
+    iterations = 3
+    b_given_a = 0.97
+    a = .005
+    not_a_given_not_b = .95
+    for i in range(3):
+        b = marginalize(b_given_a, a, not_a_given_not_b)
+        a = bayesian_probability(b_given_a, a, b)
+        print(f"Posterior: {a}")
